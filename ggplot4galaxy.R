651051545586080138c4dcd9dcf8cf0f2f8cbd67
gtrichard <26085589+gtrichard@users.noreply.github.com>
2019-07-05 13:39:23 +0200

    barebone starting

--- ggplot4galaxy.R
+++ ggplot4galaxy.R
@@ -1,24 +1,31 @@
-library('ggplot2')
+#!/usr/bin/env Rscript
 
+suppressMessages( library( library('ggplot2'))
+suppressMessages( library(library('optparse'))
 
 option_list = list(
-  
+
   make_option( c( "--input","-i" ), type="character", default=NULL,
                help="", metavar="character" ),
-  
+
   make_option( c( "--output", "-o" ), type="character", default=NULL,
                help="", metavar="character" ),
 
+  make_option( c( "--col_x" ), type="character", default=NULL,
+               help="which column to use for X axis.", metavar="character" ),
+
+  make_option( c( "--col_y" ), type="character", default=NULL,
+               help="which column to use for Y axis.", metavar="character" ),
+
   make_option( c( "--geom_boxplot"), type="character", default=NULL,
                help="Add a geom_boxplot layer", metavar="character" ),
-  
+
   make_option( c( "--geom_boxplot_outlier_shape"), type="character", default=NULL,
                help="", metavar="character" )
 )
 
 opt_parser = OptionParser( description= "ggplot2 for galaxy", option_list=option_list );
 opt = parse_args( opt_parser );
-
 
 if ( is.null( opt$input ) ) {
   print_help( opt_parser )
@@ -30,8 +37,9 @@ if ( is.null( opt$output ) ) {
   stop( "An output prefix must be supplied", call.=FALSE )
 }
 
+input_data <- read.table(opt$input, header=T)
 
-p <- ggplot(data = input,aes(x = col_x, y = col_y))
+p <- ggplot(data = input_data, aes(x = col_x, y = col_y))
 
 if (is.null(opt$geom_boxplot)==FALSE){
   p + geom_boxplot()
