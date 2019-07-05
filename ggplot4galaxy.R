46ebd8d15889dacf368057071a2e7ef8bf07f106
gtrichard <26085589+gtrichard@users.noreply.github.com>
2019-07-05 13:34:23 +0200

    start new Rscript

---
+++ ggplot4galaxy.R
@@ -0,0 +1,38 @@
+library('ggplot2')
+
+
+option_list = list(
+  
+  make_option( c( "--input","-i" ), type="character", default=NULL,
+               help="", metavar="character" ),
+  
+  make_option( c( "--output", "-o" ), type="character", default=NULL,
+               help="", metavar="character" ),
+
+  make_option( c( "--geom_boxplot"), type="character", default=NULL,
+               help="Add a geom_boxplot layer", metavar="character" ),
+  
+  make_option( c( "--geom_boxplot_outlier_shape"), type="character", default=NULL,
+               help="", metavar="character" )
+)
+
+opt_parser = OptionParser( description= "ggplot2 for galaxy", option_list=option_list );
+opt = parse_args( opt_parser );
+
+
+if ( is.null( opt$input ) ) {
+  print_help( opt_parser )
+  stop( "An input file must be supplied", call.=FALSE )
+}
+
+if ( is.null( opt$output ) ) {
+  print_help( opt_parser )
+  stop( "An output prefix must be supplied", call.=FALSE )
+}
+
+
+p <- ggplot(data = input,aes(x = col_x, y = col_y))
+
+if (is.null(opt$geom_boxplot)==FALSE){
+  p + geom_boxplot()
+}
